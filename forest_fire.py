5367636e78f7abc711e35551922326f7fbd97d37
Alexandre Heuillet <ale.heuillet@gmail.com>
2020-01-14 18:57:29 +0100

    Nearly working

--- forest_fire.py
+++ forest_fire.py
@@ -1,4 +1,6 @@
-import sys, math, time
+import sys
+import math
+import time
 import pygame
 import pygame.draw
 import numpy as np
@@ -10,6 +12,7 @@ __firecolor__ = (255, 0, 0)
 __treecolor__ = (0, 255, 0)
 __burntcolor__ = (255, 255, 255)
 
+
 def getColorCell(n):
     if n == 0:
         return __treecolor__
@@ -17,6 +20,7 @@ def getColorCell(n):
         return __firecolor__
     else:
         return __burntcolor__
+
 
 class Forest:
     def __init__(self, fire_number, size, p=0.5):
@@ -47,12 +51,11 @@ class Forest:
             for j in range(self.size):
                 if self.grid[i, j] == 1:
                     self.grid[i, j] = 2
-        for a,b in to_update:
+        for a, b in to_update:
             self.grid[a, b] = 1
             if a == self.size-1:
                 self.propagated = True
         return (len(to_update) == 0)
-        
 
     def is_valid(self, vi, vj):
         if vi < 0 or vj < 0 or vi >= self.size or vj >= self.size:
@@ -67,9 +70,10 @@ class Forest:
                 if self.grid[i, j] == 0:
                     nb_tree += 1
         return nb_tree
-    
+
     def get_stats(self):
         pass
+
 
 class Scene:
     _grid = None
@@ -77,80 +81,67 @@ class Scene:
 
     def __init__(self, perco, nb_fires, size, render=True):
         self._grid = Forest(nb_fires, size, perco)
-        self._screen_size = (int(self._grid.size*61.5),int(self._grid.size*60.9))
+        self._screen_size = (int(self._grid.size*61.5),
+                             int(self._grid.size*60.9))
         if render:
             pygame.init()
             self._screen = pygame.display.set_mode(self._screen_size)
-            self._font = pygame.font.SysFont('Arial',25)
+            self._font = pygame.font.SysFont('Arial', 25)
 
     def drawMe(self):
-        self._screen.fill((128,128,128))
+        self._screen.fill((128, 128, 128))
         for x in range(self._grid.size):
             for y in range(self._grid.size):
-                pygame.draw.rect(self._screen, 
-                        getColorCell(self._grid.grid.item((x,y))),
-                        (x*__cellSize__ + 1, y*__cellSize__ + 1, __cellSize__-2, __cellSize__-2))
+                pygame.draw.rect(self._screen,
+                                 getColorCell(self._grid.grid.item((x, y))),
+                                 (x*__cellSize__ + 1, y*__cellSize__ + 1, __cellSize__-2, __cellSize__-2))
 
-
-    def drawText(self, text, position, color = (255,64,64)):
-        self._screen.blit(self._font.render(text,1,color),position)
+    def drawText(self, text, position, color=(255, 64, 64)):
+        self._screen.blit(self._font.render(text, 1, color), position)
 
     def update(self):
         return self._grid.step()
 
-def compute_perco_threshold(perco_coeffs, tree_left):
+
+def compute_perco_threshold(perco_coeffs, perco_threshold_vals):
+    best = 0
+    index = 0
+    for i in range(1, len(perco_threshold_vals)):
+        diff = abs(perco_threshold_vals[i] - perco_threshold_vals[i-1])
+        if diff > best:
+            best = diff
+            index = i
+    return ((perco_coeffs[index] + perco_coeffs[index-1])/2)
 
 
 def plot_result(results):
+    plt.ion()
     plt.plot(results["perco_coeff"], results["tree_left"])
     plt.xlabel("Densité")
     plt.ylabel("Coefficient de percolation")
-    results["perco_threshold"] = compute_perco_threshold()
     plt.show()
 
-def fast_simulation(epochs=1, nb_fires=1, size=25):
-    results = {"perco_coeff": [], "tree_left": [], "time_to_complete": []}
-    pmax = 1
-    delta = 0.05
-    for e in range(epochs):
-        results["perco_coeff"] = []
-        results["tree_left"].append([])
-        p = 0
-        while p < pmax:
-            scene = Scene(p, nbfires, size, render=False)
-            done = False
-            while done == False:
-                #clock.tick(30)
-                #scene.drawMe()
-                done = scene.update()
-                #pygame.display.flip()
-            results["perco_coeff"].append(p)
-            results["tree_left"][e].append(scene._grid.get_trees_left()/(scene._grid.size**2))
-            p += delta
-            #pygame.quit()
-    plot_result(results)
 
-def sim_with_grid(size=25, nb_fires=1, perco=0.5):
-    clock = pygame.time.Clock()
-    scene = Scene(p, nb_fires, size)
-    done = False
-    while done == False:
-        clock.tick(30)
-        scene.drawMe()
-        done = scene.update()
-        pygame.display.flip()
-    pygame.quit()
+def analyze_results(results):
+    results["steps"] = np.mean(results["steps"])
+    results["mean_tree_left"] = np.mean(results["tree_left"])
+    results["perco_threshold"] = compute_perco_threshold(
+        results["perco_coeff"], results["tree_left"])
+    return results
 
-#merge both functions into one
-def simulate(epochs=1, nb_fires=1, size=25, p_start=0, pmax=1, delta=0.05, graphical_rendering=True):
-    results = {"perco_coeff": [], "tree_left": [], "time_to_complete": [], "steps": [], "perco_threshold": []}
+
+def simulate(epochs=1, nb_fires=1, size=25, p_start=0, p_max=1, delta=0.05, graphical_rendering=True):
+    results = {"perco_coeff": [], "tree_left": [],
+               "steps": [], "mean_tree_left": 0}
     clock = pygame.time.Clock()
-    for e in range(epochs):
-        results["perco_coeff"] = []
+    results["perco_coeff"] = []
+    i = 0
+    p = p_start
+    while p < p_max:
         results["tree_left"].append([])
-        p = p_start
-        while p < pmax:
-            scene = Scene(p, nbfires, size, render=graphical_rendering)
+        results["steps"].append([])
+        for e in range(epochs):
+            scene = Scene(p, nb_fires, size, render=graphical_rendering)
             done = False
             reached_propagation = False
             t1 = time.time()
@@ -161,15 +152,15 @@ def simulate(epochs=1, nb_fires=1, size=25, p_start=0, pmax=1, delta=0.05, graph
                     scene.drawMe()
                     pygame.display.flip()
                 done = scene.update()
-                if scene._grid.propagated and !reached_propagation:
-                    results["perco_threshold"].append(time.time() - t1)
-                    reached_propagation = True
                 s += 1
-            results["time_to_complete"].append(time.time() - t1)
-            results["steps"].append(s)
-            results["perco_coeff"].append(p)
-            results["tree_left"][e].append(scene._grid.get_trees_left()/(scene._grid.size**2))
-            p += delta
-            if graphical_rendering:
-                pygame.quit()
-    return plot_result(results)
+            results["steps"][i].append(s)
+            results["tree_left"][i].append(
+                scene._grid.get_trees_left()/(scene._grid.size**2))
+        results["perco_coeff"].append(p)
+        results["tree_left"][i] = np.mean(results["tree_left"][i])
+        results["steps"][i] = np.mean(results["steps"][i])
+        p += delta
+        i += 1
+    if graphical_rendering:
+        pygame.quit()
+    return analyze_results(results)
--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 from random import randint
-from forest_fire import sim_with_grid, fast_simulation
+from forest_fire import simulate, plot_result
 
 sg.ChangeLookAndFeel('Dark')
 
@@ -10,6 +10,7 @@ main_layout = [
     [sg.Text('Percolation coefficient (for PyGame simulation only)', size=(15, 1)), sg.InputText('0.5')],
     [sg.Checkbox('Fast Simulation (no PyGame animations)')],
     [sg.Text('Number of epochs (for fast simulation only)', size=(15, 1)), sg.InputText('1')],
+    [sg.Text('Percolation delta (for fast simulation only)', size=(15, 1)), sg.InputText('1')],
     [sg.Submit()]
 ]
 
@@ -17,22 +18,23 @@ main_layout = [
 main_window = sg.Window('Forest Fire Simulator').Layout(main_layout)
 button, values = main_window.Read()
 
-if values[3] == True:
-    fast_simulation(epochs=values[4], nb_fires=values[1], size=values[0])
+if values[3]:
+    results = simulate(epochs=int(values[4]), nb_fires=int(values[1]), size=int(values[0]), p_start=0, p_max=1, delta=float(values[5]), graphical_rendering=False)
 else:
-    sim_with_grid(size=values[0], nb_fires=values[1], perco=values[2])
+    results = simulate(nb_fires=int(values[1]), size=int(values[0]), p_start=float(values[2]), p_max=float(values[2])+1, delta=1)
 
 
 
 result_layout = [
     [sg.Text('Results', justification='center', font='Helvetica 20', auto_size_text=True)],
-    [sg.Text('Mean tree density left: '), sg.Text(str(population.current_gen+1))],
-    [sg.Text('Mean time to propagate from left to right: '), sg.Text(str(strong))],
-    [sg.Text('Mean number of steps to complete: '), sg.Text(str(weak))],
-    [sg.Text('Detected percolation threshold: '),  sg.Text()]
+    [sg.Text('Mean tree density left: '), sg.Text(str(results["mean_tree_left"]))],
+    [sg.Text('Mean number of steps to complete: '), sg.Text(str(results["steps"]))],
+    [sg.Text('Detected percolation threshold: '),  sg.Text(str(results["perco_threshold"]))],
     [sg.CloseButton(button_text='Close')]
 ]
 
 result_window = sg.Window('Results').Layout(result_layout)
+
+plot_result(results)
 
 button, values = result_window.Read()
